c7829a6f70ab6218c575b15b89da598a5a61cbd9
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-02-23 21:43:51 -0300

    --feat Saves section after possible register error.

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -9,5 +9,15 @@ exports.index = (req, res, next) => {
 exports.register = async (req, res, next) => {
   const login = new Login(req.body);
   await login.register();
-  res.send(login.errors);
+
+  if(login.errors.length > 0){
+    req.flash('errors', login.errors)
+    req.session.save(function () {
+      res.redirect('back')
+      return
+    })
+    return
+  }
+
+  res.send(login.errors)
 };
